0ac55704eb6ba57a07e4e9cead851096178b86f8
compscibaddie <601335@student.belgiumcampus.ac.za>
2025-09-19 22:42:02 +0200

    Update Main timer loop

--- timer.py
+++ timer.py
@@ -5,7 +5,8 @@ import datetime as dt
 import tkinter
 from tkinter import messagebox
 from tkinter import simpledialog
-import windsound
+import winsound
+
 
 #hinding the main window functionality
 root = tkinter.Tk()
@@ -55,33 +56,44 @@ def remove_websites(filepath):
 
     #main script information
     while True:
+        t_now = dt.datetime.now()
+    
         if t_now < t_fut:
+            # Pomodoro in progress - block websites
             add_websites(host_path)
-            print('First tnow < tfut')
-
-            if t_fut <= t_now <= t_fin:
-                print('Break time!')
-                remove_websites(host_path)
-            
-            else:
-                print('Third tnow > tfut - Finished')
-                print('\a')
-
-                for i in range(10):
-                    windsound.Beep((i+100), 500)
+            print(f"Pomodoro in progress... {t_fut - t_now} remaining")
+        
+        elif t_fut <= t_now <= t_fin:
+            # Break time - unblock websites
             remove_websites(host_path)
-            usr_ans = messagebox.askyesno("Pomodoro finished!", "Do you want to start another Pomodoro?")
+            print('Break time!')
+        
+        else:
+            # Pomodoro finished
+            remove_websites(host_path)
+            print('\a')
+            
+            for i in range(10):
+                winsound.Beep((i+100), 500)
+                
             total_pomodoros += 1
-            if usr_ans == True:
+            usr_ans = messagebox.askyesno("Pomodoro finished!", 
+                                        f"Completed {total_pomodoros} pomodoros.\nDo you want to start another Pomodoro?")
+            
+            if usr_ans:
+                # Reset timer for another pomodoro
                 t_now = dt.datetime.now()
                 t_fut = t_now + dt.timedelta(0, t_pom)
                 t_fin = t_now + dt.timedelta(0, t_pom+delta_sec)
-            continue
-            if usr_ans == False:
+                messagebox.showinfo("Pomodoro started!", 
+                                f"It is now {t_now.strftime('%H:%M')} hrs. \nTimer is set for 25 minutes")
+                continue
+            else:
                 print(f"Total pomodoros completed: {total_pomodoros}")
                 break
-        time.sleep(20)
-        t_now = dt.datetime.now()
-        timenow = t_now.strftime("%H:%M")
-        print('\n\nMade it to the end!\n\n')
+            
+    time.sleep(20)
+t_now = dt.datetime.now()
+timenow = t_now.strftime("%H:%M")
+print('\n\nMade it to the end!\n\n')
     
