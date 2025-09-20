2e554a2b19e30b3139bfef4e0192e79ad9e6bbf8
compscibaddie <601335@student.belgiumcampus.ac.za>
2025-09-20 17:29:14 +0200

    Add main timer widget

--- timer.py
+++ timer.py
@@ -34,4 +34,36 @@ class PomodoroTimer:
         
         self.root.mainloop()
 
-  
+#creating the widgets for the timer
+def create_widgets(self):
+    self.timer_label = tkk.label(self.root, text="45:00", font=("Arial", 40))
+    self.timer_label.pack(pady=20)
+
+    #timer status label
+    self.status_label = ttk.Label(self.root, text="Work Time - Focus", foreground="red") 
+    self.status_label.pack(pady=5)
+
+    #Timer Counter
+    self.counter_label = ttk.Label(self.root, text="Pomodoros Completed: 0") 
+    self.counter_label.pack(pady=5)
+
+    #Button Frame
+    button_frame = ttk.Frame(self.root)
+    button_frame.pack(pady=20)
+
+    #functional buttons
+    #Start Button
+    self.start_button = ttk.Button(button_frame, text="Start", command=self.start_timer)
+    self.start_button.grid(row=0, column=0, padx=10)
+
+    #Pause Button
+    self.pause_button = ttk.Button(button_frame, text="Pause", command=self.pause_timer, state="disabled")
+    self.pause_button.grid(row=0, column=1, padx=10)
+
+    #Reset Button
+    self.timer_button = tkk.button(button_frame, text="Reset", command=self.reset_timer)
+    self.reset_button.grid(row=0, column=3, padx=10)
+
+    #Skip Button
+    self.skip_button = tkk.button(button_frame, text="Skip", command=self.skip_timer)
+    self.skip_button.grid(row=0, column=3, padx=10)
